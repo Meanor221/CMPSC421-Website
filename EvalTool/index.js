51505891fe67989293cf44c51d819bb006356a84
Chris Andrejewski <christopher.andrejewski@gmail.com>
2016-02-17 21:22:53 -0500

    good enough

--- EvalTool/index.js
+++ EvalTool/index.js
@@ -13,21 +13,23 @@ router.get('/', function(req, res, next) {
 
 router.get('/questions/:id', function(req, res, next) {
   var id = parseInt(req.params.id, 10);
-  var answer = clientKey[id];
+  var answer = clientKey[id-1];
   fs.readFile(path.join(__dirname, 'Q'+id+'.html'), 'utf8', function(error, data) {
     if(error) return next(error);
-    res.send(data
-      .replace("value='"+id+"'", "value='"+id+"' checked")
-      .replace('value="'+id+'"', 'value="'+id+'" checked'));
+    var html = data
+      .replace("value='"+answer+"'", "value='"+answer+"' checked")
+      .replace('value="'+answer+'"', 'value="'+answer+'" checked');
+    res.send(html);
   });
 });
 
 router.post('/questions/:id', function(req, res, next) {
   var id = parseInt(req.params.id, 10);
-  clientKey[id] = req.body.answer;
-  var nextId = id + 1;
-  if(nextId >= 10) return res.redirect('/submit');
-  res.redirect('/questions/'+nextId);
+  clientKey[id-1] = req.body.ans;
+  var toPrevious = req.body.direction.charAt(0).toLowerCase() === 'p';
+  var nextId = id + (toPrevious ? -1 : 1);
+  if(nextId > 10) return res.redirect('/EvalTool/submit');
+  res.redirect('/EvalTool/questions/'+nextId);
 });
 
 var tScore = 0;
@@ -38,9 +40,10 @@ router.get('/submit', function(req, res, next) {
     .reduce(function(score, correct) {
       return correct ? (score + 1) : score;
     });
-  fs.readFile('submit.html', 'utf8', function (error, data) {
+  tScore = score;
+  clientKey = '          '.split('');
+  fs.readFile(path.join(__dirname, 'submit.html'), 'utf8', function(error, data) {
 		if (error) return next(error);
-    tScore = score;
 	  res.send(data.replace('$SCORE$', score));
 	});	
 });
@@ -56,15 +59,44 @@ router.post('/submit', function(req, res, next) {
     toEmail: req.body.temail,
     subject: req.body.subject, 
   };
-  var message = {
-    from: [form.fromName, ' <', form.fromEmail, '>'].join(''),
-    to: [form.toName, ' <', form.toEmail, '>'].join(''),
-    subject: form.subject,
-    text: [form.formName, ' scored ', tScore, '/', answerKey.length].join(''),
-  };
-  emailTransport.sendMail(message, function(error) {
+  var messages = emails(form);
+  var dev = true;
+  (dev ? mockEmail : email)(messages[0], messages[1], function(error) {
     if(error) return next(error);
-    res.sendFile('email-sent.html');
+    tScore = null;
+    res.sendFile(path.join(__dirname, 'submitted.html'));
   });
 });
+
+function emails(form) {
+  var teacherMessage = {
+    from: [form.fromName, ' <', form.fromEmail, '>'].join(''),
+    to: form.toEmail,
+    subject: form.subject,
+    text: [form.fromName, ' scored ', tScore, '/', answerKey.length].join(''),
+  };
+  var studentMessage = {
+    from: form.fromEmail,
+    to: form.fromEmail,
+    subject: 'Your evaluation score',
+    text: [form.fromName, ' scored ', tScore, '/', answerKey.length].join(''),
+  };
+  return [teacherMessage, studentMessage];
+}
+
+function email(msg1, msg2, next) {
+  emailTransport.sendMail(msg1, function(error) {
+    if(error) return next(error);
+    emailTransport.sendMail(msg2, function(error) {
+      if(error) return next(error);
+      next(null);
+    });
+  });
+} 
+
+function mockEmail(msg1, msg2, next) {
+  console.log(msg1);
+  console.log(msg2);
+  next(null);
+}
 
