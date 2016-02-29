d606f525ea15c506f600d96b9aa0f27184b442da
Meanor221 <meanor99@hotmail.com>
2016-02-29 15:10:20 -0500

    Added schedule.js
    schedule.js contains the array with the schedule information.

---
+++ Schedule/schedule.js
@@ -0,0 +1,353 @@
+var schedule[44][5];
+//Class 1
+schedule[0][0]="1";
+schedule[0][1]="M: 1/11";
+schedule[0][2]="JScript"
+schedule[0][3]="Introduction to Web Applications"
+schedule[0][4]="Topic: Course Web Application"
+schedule[0][5]="";
+
+//Class 2
+schedule[1][0]="2";
+schedule[1][1]="W: 1/13";
+schedule[1][2]="JScript"
+schedule[1][3]="Fundamental Javascript"
+schedule[1][4]=""
+schedule[1][5]="David 2-8";
+
+//Class 3
+schedule[2][0]="3";
+schedule[2][1]="F: 1/15";
+schedule[2][2]="JScript";
+schedule[2][3]="Regular Expressions";
+schedule[2][4]="";
+schedule[2][5]="David 10";
+
+//Week 2
+//Class 4
+schedule[3][0]="4";
+schedule[3][1]="W: 1/20";
+schedule[3][2]="JScript"
+schedule[3][3]="Server-Side JavaScript"
+schedule[3][4]=""
+schedule[3][5]="David 12";
+
+//Class 5
+schedule[4][0]="";
+schedule[4][1]="F: 1/22";
+schedule[4][2]="JScript"
+schedule[4][3]="Lab1: Node.js"
+schedule[4][4]="Node server"
+schedule[4][5]="Quiz1";
+
+//Class 6
+schedule[5][0]="5";
+schedule[5][1]="M: 1/25";
+schedule[5][2]="JScript"
+schedule[5][3]="JavaScript in Web Browsers"
+schedule[5][4]=""
+schedule[5][5]="David 13";
+
+//Class 7
+schedule[6][0]="6";
+schedule[6][1]="W: 1/27";
+schedule[6][2]="JScript"
+schedule[6][3]="JavaScript: the Window object"
+schedule[6][4]=""
+schedule[6][5]="David 14";
+
+//Class 8
+schedule[7][0]="";
+schedule[7][1]="F: 1/29";
+schedule[7][2]="JScript"
+schedule[7][3]="Lab2: Node.js"
+schedule[7][4]="Simple web site"
+schedule[7][5]="Quiz2";
+
+//Class 9
+schedule[8][0]="7";
+schedule[8][1]="M: 2/1";
+schedule[8][2]="HTML & CSS"
+schedule[8][3]="Scripting Document: the model"
+schedule[8][4]="Scripting client"
+schedule[8][5]="David 15";
+
+//Class 10
+schedule[9][0]="8";
+schedule[9][1]="W: 2/3";
+schedule[9][2]="HTML & CSS"
+schedule[9][3]="Scripting CSS: the view"
+schedule[9][4]="CSS desing"
+schedule[9][5]="David 16";
+
+//Class 11
+schedule[10][0]="";
+schedule[10][1]="F: 2/5";
+schedule[10][2]="HTML & CSS"
+schedule[10][3]="Lab3: Node.js"
+schedule[10][4]="Syllabus TOC"
+schedule[10][5]="Quiz3";
+
+//Class 12
+schedule[11][0]="9";
+schedule[11]="M: 2/8";
+schedule[11][2]="Event"
+schedule[11][3]="Advanced Javascript: event handling"
+schedule[11][4]="Scripting client"
+schedule[11][5]="David 17";
+
+//Class 13
+schedule[12][0]="10";
+schedule[12][1]="W: 2/10";
+schedule[12][2]="Event"
+schedule[12][3]="Advanced Javascript: event handling"
+schedule[12][4]="Scripting client"
+schedule[12][5]="David 17";
+
+//Class 14
+schedule[13][0]="";
+schedule[13][1]="F: 2/12";
+schedule[13][2]="Event"
+schedule[13][3]="Lab4: Node.js"
+schedule[13][4]="Simple Evaluation tool"
+schedule[13][5]="Quiz4";
+
+//Class 15
+schedule[14][0]="11";
+schedule[14][1]="M: 2/15";
+schedule[14][2]="Ajax"
+schedule[14][3]="Advanced Javascript: Ajax I"
+schedule[14][4]="Ajax componenet"
+schedule[14][5]="David 18";
+
+//Class 16
+schedule[15][0]="12";
+schedule[15][1]="W: 2/17";
+schedule[15][2]="Ajax"
+schedule[15][3]="Advanced Javascript: Ajax II"
+schedule[15][4]="Ajax componenet"
+schedule[15][5]="David 18";
+
+//Class 17
+schedule[16][0]="";
+schedule[16][1]="F: 2/19";
+schedule[16][2]="Ajax"
+schedule[16][3]="Lab5: Node.js"
+schedule[16][4]="Evaluation tool upon Ajax"
+schedule[16][5]="Quiz5";
+
+//Class 18
+schedule[17][0]="13";
+schedule[17][1]="M: 2/22";
+schedule[17][2]="jQuery"
+schedule[17][3]="Advanced Javascript: jQuery I"
+schedule[17][4]="Use jQuery"
+schedule[17][5]="David 19";
+
+//Class 19
+schedule[18][0]="14";
+schedule[18][1]="W: 2/24";
+schedule[18][2]="jQuery"
+schedule[18][3]="Advanced Javascript: jQuery II"
+schedule[18][4]="Use jQuery"
+schedule[18][5]="David 19";
+
+//Class 20
+schedule[19][0]="";
+schedule[19][1]="F: 2/26";
+schedule[19][2]="jQuery"
+schedule[19][3]="Lab6: Node.js"
+schedule[19][4]="Construct schedule table"
+schedule[19][5]="Quiz6";
+
+//Class 21
+schedule[20][0]="15";
+schedule[20][1]="M: 2/29";
+schedule[20][2]="HTML Media & Graphics"
+schedule[20][3]="Scripting Images"
+schedule[20][4]=""
+schedule[20][5]="David 21";
+
+//Class 22
+schedule[21][0]="16";
+schedule[21][1]="W: 3/2";
+schedule[21][2]="HTML Media & Graphics"
+schedule[21][3]="Graphics in canvas"
+schedule[21][4]=""
+schedule[21][5]="david 21";
+
+//Class 23
+schedule[22][0]="";
+schedule[22][1]="F: 3/4";
+schedule[22][2]="HTML Media & Graphics"
+schedule[22][3]="Lab7: Node.js"
+schedule[22][4]="UML class editor"
+schedule[22][5]="Quiz7";
+
+//Class 24
+schedule[23][0]="17";
+schedule[23][1]="M: 3/14";
+schedule[23][2]="HTML5 API"
+schedule[23][3]="Web Workers"
+schedule[23][4]=""
+schedule[23][5]="David 22";
+
+//Class 25
+schedule[24][0]="18";
+schedule[24][1]="W: 3/16";
+schedule[24][2]="HTML5 API"
+schedule[24][3]="Web Sockets"
+schedule[24][4]=""
+schedule[24][5]="David 22";
+
+//Class 26
+schedule[25][0]="";
+schedule[25][1]="F: 3/18";
+schedule[25][2]="Lab8: NOde.js"
+schedule[25][3]="Lab8: Node.js"
+schedule[25][4]="Group Chat"
+schedule[25][5]="Quiz8";
+
+//Class 27
+schedule[26][0]="19";
+schedule[26][1]="M: 3/21";
+schedule[26][2]="JSP"
+schedule[26][3]="Fundamental JSP"
+schedule[26][4]="Use GlassFish inside NetBeans"
+schedule[26][5]="";
+
+//Class 28
+schedule[27][0]="20";
+schedule[27][1]="W: 3/23";
+schedule[27][2]="JSP"
+schedule[27][3]="JSP & JavaBeans"
+schedule[27][4]=""
+schedule[27][5]="Martin ch1";
+
+//Class 29
+schedule[28][0]="";
+schedule[28][1]="F: 3/25";
+schedule[28][2]="JSP"
+schedule[28][3]="Lab9: JSP"
+schedule[28][4]="Roster JSP with JavaBeans"
+schedule[28][5]="Quiz9";
+
+//Class 30
+schedule[29][0]="21";
+schedule[29][1]="M: 3/28";
+schedule[29][2]="REST"
+schedule[29][3]="JSP & MVC"
+schedule[29][4]=""
+schedule[20][5]="Martin ch1";
+
+//Class 31
+schedule[30][0]="22";
+schedule[30][1]="W: 3/30";
+schedule[30][2]="REST"
+schedule[30][3]="RESTful Web Services: HttpServlet"
+schedule[30][4]="Intro to REST principles"
+schedule[30][5]="Martin ch2";
+
+//Class 32
+schedule[31][0]="";
+schedule[31][1]="F: 4/1";
+schedule[31][2]=""
+schedule[31][3]="Lab10: RESTful Web Services (MVC)"
+schedule[31][4]="RESTful service with MVC"
+schedule[31][5]="";
+
+//Class 33
+schedule[32][0]="23";
+schedule[32][1]="M: 4/4";
+schedule[32][2]="REST"
+schedule[32][3]="RESTful Web Services: jTable & DB"
+schedule[32][4]=""
+schedule[32][5]="";
+
+//Class 34
+schedule[33][0]="24";
+schedule[33][1]="W: 4/6";
+schedule[33][2]="REST"
+schedule[33][3]="RESTful Web Services: Restlet"
+schedule[33][4]=""
+schedule[33][5]="Martin ch2";
+
+//Class 35
+schedule[34][0]="";
+schedule[34][1]="F: 4/8";
+schedule[34][2]="REST"
+schedule[34][3]="Lab11: Restful Web Services (jTable)"
+schedule[34][4]="RESTful service with jTable UI"
+schedule[34][5]="";
+
+//Class 36
+schedule[35][0]="25";
+schedule[35][1]="M: 4/11";
+schedule[35][2]="REST"
+schedule[35][3]="Consuming RESTful Services on the Web"
+schedule[35][4]="Javascript client to Pub. REST"
+schedule[35][5]="Martin ch3";
+
+//Class 37
+schedule[36][0]="26";
+schedule[36][1]="W: 4/13";
+schedule[36][2]="REST"
+schedule[36][3]="Lab12: Real world RESTful Web Services"
+schedule[36][4]=""
+schedule[36][5]="Martin ch3";
+
+//Class 38
+schedule[37][0]="";
+schedule[37][1]="F: 4/15";
+schedule[37][2]="REST"
+schedule[37][3]="Work on project"
+schedule[37][4]="Teamwork Calendar"
+schedule[37][5]="";
+
+//Class 39
+schedule[38][0]="27";
+schedule[38][1]="M: 1/11";
+schedule[38][2]="Project"
+schedule[38][3]="Work on project"
+schedule[38][4]=""
+schedule[38][5]="";
+
+//Class 40
+schedule[39][0]="28";
+schedule[39][1]="W: 4/20";
+schedule[39][2]="Project"
+schedule[39][3]="Work on project"
+schedule[39][4]=""
+schedule[39][5]="";
+
+//Class 41
+schedule[40][0]="";
+schedule[40][1]="F: 4/22";
+schedule[40][2]="Project"
+schedule[40][3]="Work on project"
+schedule[40][4]=""
+schedule[40][5]="";
+
+//Class 42
+schedule[41][0]="";
+schedule[41][1]="M: 4/25";
+schedule[41][2]="Project"
+schedule[41][3]="Final project presentation"
+schedule[41][4]=""
+schedule[41][5]="";
+
+//Class 43
+schedule[42][0]="";
+schedule[42][1]="W: 4/27";
+schedule[42][2]="Project"
+schedule[42][3]="Final project presentation"
+schedule[42][4]=""
+schedule[42][5]="";
+
+//Class 44
+schedule[43][0]="";
+schedule[43][1]="F: 4/29";
+schedule[43][2]="Project"
+schedule[43][3]="Final project presentation"
+schedule[43][4]=""
+schedule[43][5]="";
